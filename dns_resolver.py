3ed41a78409a0f90cf82219338aab7aabd8ca6fd
Ismael Correa <ismael.correa.25@gmail.com>
2022-04-08 15:36:53 -0400

    first working version, cache missing

--- dns_resolver.py
+++ dns_resolver.py
@@ -1,35 +1,65 @@
 from utilities import *
 import socket
-from dnslib import DNSRecord
+from dnslib import DNSRecord, DNSHeader, DNSQuestion, RR, A
 
 class DNSResolver:
 
-  def __recv_domain_name_dns_msg(socket: socket) -> str:
+  def __recv_domain_name_dns_msg(socket: socket) -> tuple[str, str, str]:
 
-    data, _ = socket.recvfrom(4096)
+    data, client_addr = socket.recvfrom(4096)
     d = DNSRecord.parse(data)
+    dom_id = d.header.id
     first_query = d.get_q() # primer objeto en la lista all_querys
     domain_name_in_query = first_query.get_qname() # nombre de dominio por el cual preguntamos
 
-    return str(domain_name_in_query)
+    return str(domain_name_in_query), client_addr, dom_id
 
   @classmethod
   def run(cls) -> None:
 
     dgram_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     dgram_socket.bind(('127.0.0.1', 5354))  # CAMBIAR PUERTO A 5353 ANTES DE ENTREGAR
+    root_addr = '8.8.8.8'
 
     while True:
 
-      domain_name = cls.__recv_domain_name_dns_msg(dgram_socket)
+      domain_name, client_addr, dom_id = cls.__recv_domain_name_dns_msg(dgram_socket)
       links_in_request = parse_domain(domain_name)
 
       i = 0
-      addr = '192.33.4.12'
-      while i < len(links_in_request):
+      addr = root_addr
+      curr_link = links_in_request[i]
+      while i < len(links_in_request) - 1:
 
-        curr_link = links_in_request[i]
-        d = send_dns_message()
+        d = send_dns_message(curr_link, addr)
+        d = DNSReply(d)
+
+        if d.responds_with_ip:
+          i += 1
+          curr_link = links_in_request[i]
+          addr = d.data
+        
+        else:
+          curr_link = d.data
+          addr = root_addr
+      
+      d = send_dns_message(curr_link, addr)
+      d = DNSReply(d)
+      
+      if not d.responds_with_ip:
+
+        d = send_dns_message(d.data, root_addr)
+        d = DNSReply(d)
+
+      domain_ip = d.data
+
+      resp = DNSRecord(DNSHeader(id=dom_id, qr=1,aa=1,ra=1),
+                       q=DNSQuestion(domain_name),
+                       a=RR(domain_name,rdata=A(domain_ip)))
+
+      dgram_socket.sendto(bytes(resp.pack()), client_addr)
+
+      print(domain_name, 'corresponde a la IP: ', domain_ip)
 
 
 
--- utilities.py
+++ utilities.py
@@ -1,9 +1,11 @@
 from operator import sub
+from pickle import TRUE
 import socket
 from dnslib import DNSRecord
+import dnslib
 from dnslib.dns import CLASS, QTYPE
 
-def send_dns_message(query_name, address):
+def send_dns_message(query_name: str, address: str) -> DNSRecord:
   # Acá ya no tenemos que crear el encabezado porque dnslib lo hace por nosotros, por default pregunta por el tipo A
   qname = query_name
   q = DNSRecord.question(qname)
@@ -50,19 +52,25 @@ class DNSReply:
     number_of_answer_elements = dnslib_reply.header.a
     number_of_authority_elements = dnslib_reply.header.auth
 
-    self.soa = number_of_answer_elements > 0
+    self.responds_with_ip = False
 
     # answer section
     if number_of_answer_elements > 0:
       first_answer = dnslib_reply.get_a() # primer objeto en la lista all_resource_records
       answer_rdata = first_answer.rdata # rdata asociada a la respuesta
-      self.data = answer_rdata
+      self.responds_with_ip = True
+      self.data = str(answer_rdata)
 
     # authority section
     if number_of_authority_elements > 0:
       authority_section_list = dnslib_reply.auth # contiene un total de number_of_authority_elements
       authority_section_RR_0 = authority_section_list[0] # objeto tipo dnslib.dns.RR
-      authority_section_SOA = authority_section_RR_0.rdata # si recibimos auth_type = 'SOA' este es un objeto tipo dnslib.dns.SOA
-      primary_name_server = authority_section_SOA.get_mname() # servidor de nombre primario
-      self.data = primary_name_server
-
+      authority_section_0_rdata = authority_section_RR_0.rdata
+      # si recibimos auth_type = 'SOA' este es un objeto tipo dnslib.dns.SOA
+      if isinstance(authority_section_0_rdata, dnslib.dns.SOA):
+        primary_name_server = authority_section_0_rdata.get_mname()  # servidor de nombre primario
+        self.data = str(primary_name_server)
+    
+      elif isinstance(authority_section_0_rdata, dnslib.dns.NS) and not self.responds_with_ip: # si en vez de SOA recibimos un registro tipo NS
+        name_server_domain = authority_section_0_rdata # entonces authority_section_0_rdata contiene el nombre de dominio del primer servidor de nombre de la lista
+        self.data = str(name_server_domain)
